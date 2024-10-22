71b5c575c1ff26b2616b9ecb571f0783504a4f60
deflex82 <susantrimal77@gmail.com>
2024-10-22 15:43:33 +0545

    file uploaded

---
+++ server.js
@@ -0,0 +1,30 @@
+const io=require("socket.io")(3001,{cors:{
+    origin:"*",
+    methods:["GET","POST"],
+}});
+
+
+
+io.on("connection",(socket)=>{
+    socket.on("joinRoom", (fixtureId) => {
+        socket.join(fixtureId);
+        console.log(`User joined room: ${fixtureId}`);
+    });
+    socket.on("message", (data) => {
+        const { fixtureId, message ,sender,sendergameusername,imageUrl} = data;
+       const timestamp= new Date().toISOString();
+        const chat = {message,sender,sendergameusername,timestamp,imageUrl}
+        // Emit the message to the specific room (fixture)
+        io.to(fixtureId).emit("message", chat);
+        console.log("send to room",chat);
+    });
+
+
+
+
+    
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+})
+
